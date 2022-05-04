e75578250203ac4221a68c6cbb0918fb8ecbccad
Anthony Castle <acastle@Anthonys-MacBook-Pro.local>
2022-05-04 10:54:37 +0200

    add padding at start of month and identify weekend days

--- calendar.js
+++ calendar.js
@@ -1,14 +1,30 @@
-const calendar = document.querySelector("#calendarContainer");
+const calendar = document.querySelector("#calendar");
 
 const isWeekend = (day) => {
-  return true;
+  return day % 7 == 6 || day % 7 == 0 ? true : false;
 };
 
-for (let day = 1; day < 32; day++) {
-  const weekend = isWeekend(day);
+const dt = new Date();
+const year = dt.getFullYear();
+const month = 6; //dt.getMonth();
+const day = dt.getDay();
+
+const firstDayofMonth = new Date(year, month, 1);
+paddingDays = firstDayofMonth.getDay();
+
+const noDaysInMonth = new Date(year, month + 1, 0).getDate();
+console.log("no of days: ", noDaysInMonth);
+
+console.log(firstDayofMonth);
+console.log("padding: ", paddingDays);
+
+for (let day = 0; day < paddingDays + noDaysInMonth; day++) {
+  const weekend = isWeekend(day + 1);
   calendar.insertAdjacentHTML(
     "beforeend",
-    `<div class="day ${weekend && "weekend"}">${day}</div>`
+    `<div class="${day < paddingDays ? "unavailable" : "day"} ${
+      weekend && "weekend"
+    }">${day - paddingDays + 1}</div>`
   );
 }
 
@@ -17,3 +33,5 @@ document.querySelectorAll(".day").forEach((day) => {
     event.currentTarget.classList.toggle("selected");
   });
 });
+
+console.log("13%7", 13 % 7);
